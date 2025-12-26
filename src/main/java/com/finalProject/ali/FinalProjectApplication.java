9d72c483a82c2aee3cbd196cd8853cc91b6d6e84
hjin9473 <hjin9473@gmail.com>
2025-12-26 23:56:39 +0900

    [fix] users오류, roomDTO 서치 실패 문제 해결

--- src/main/java/com/finalProject/ali/FinalProjectApplication.java
+++ src/main/java/com/finalProject/ali/FinalProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.finalProject.ali.user.dao")
+@MapperScan("com.finalProject.ali")
 public class FinalProjectApplication {
 
     public static void main(String[] args) {
