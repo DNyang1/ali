b945c484d7d5b6ab604d33d5c4cf8339ec574f87
hyunsungSm <oohs942230@naver.com>
2025-12-29 10:40:18 +0900

    [feat] index화면, 카테고리 분류 기능 추가

---
+++ src/main/java/com/finalProject/ali/products/dto/CategoryDTO.java
@@ -0,0 +1,10 @@
+package com.finalProject.ali.products.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CategoryDTO {
+    private String categoryId;
+    private String categoryName;
+}
