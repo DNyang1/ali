90d78515c0128b09b813c9322bf546ba142f7924
DNyang1 <rkddnwn3210@gmail.com>
2026-01-07 11:15:31 +0900

    [feat] 결제 결과 order complete 추가

--- src/main/java/com/finalProject/ali/order/controller/OrderController.java
+++ src/main/java/com/finalProject/ali/order/controller/OrderController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/orders")
+@RequestMapping("/api/order")
 public class OrderController {
 
     private final OrderService orderService;
